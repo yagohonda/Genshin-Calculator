866240b97a00b80d7a131a6887e3c30931916177
yagohonda <37645017+yagohonda@users.noreply.github.com>
2021-12-20 00:31:19 -0300

    Atualização de banco de dados
    Versão Beta com foco em operações CRUD (Create, Read, Update, Delete). O objetivo dessa atualização foi fornecer mais estabilidade ao programa e incorporar comunicações com bancos de dados SQL, realizando registros de novos usuários além do login a partir de dados externos. Programa ainda está incompleto, é necessário incorporar rotinas para acessar o arquivo de dados CSV em caso de falha de comunicação com o servidor SQL.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-#PROBLEMAS = LOGIN NAO FUNCIONA / TERMINAR INTEFACE DA CALCULADORA / TERMINAR OS CALCULOS / AJEITAR O BANCO DE DADOS
 import sys
-import time
+import mysql.connector
+#import LoginWindow
+from datetime import datetime
 from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QLabel, QGridLayout, QSizePolicy
 from PyQt6.QtGui import QIcon, QFont
 
-
-usuario ="123"
-senha ="123"
 
 ataquebranco = 1
 ataqueverde = 1
@@ -86,14 +84,17 @@ class MainApp(QWidget):
         self.lineEdits['1a'] = QLineEdit(self)
         self.lineEdits['1a'].move(140, 35)
         self.lineEdits['1a'].resize(50, 32)
+        self.lineEdits['1a'].setText("1")
 
 
         labels['2a'] = QLabel(self)
         labels['2a'].setText('Ataque Verde')
         labels['2a'].move(10, 75)
-        self.lineEdits['2a'] = QLineEdit(self)
+        self.lineEdits['2a'] = QLineEdit(self) #QlineDefaut value
         self.lineEdits['2a'].move(140, 75)
         self.lineEdits['2a'].resize(50, 32)
+        self.lineEdits['2a'].setText("1")
+
 
         labels['3a'] = QLabel(self)
         labels['3a'].setText('Taxa crítica')
@@ -101,6 +102,7 @@ class MainApp(QWidget):
         self.lineEdits['3a'] = QLineEdit(self)
         self.lineEdits['3a'].move(140, 115)
         self.lineEdits['3a'].resize(50, 32)
+        self.lineEdits['3a'].setText("1")
 
         labels['4a'] = QLabel(self)
         labels['4a'].setText('Dano Crítico')
@@ -108,6 +110,7 @@ class MainApp(QWidget):
         self.lineEdits['4a'] = QLineEdit(self)
         self.lineEdits['4a'].move(140, 155)
         self.lineEdits['4a'].resize(50, 32)
+        self.lineEdits['4a'].setText("1")
 
         labels['5a'] = QLabel(self)
         labels['5a'].setText('Bonus ataque normal')
@@ -115,6 +118,7 @@ class MainApp(QWidget):
         self.lineEdits['5a'] = QLineEdit(self)
         self.lineEdits['5a'].move(140, 195)
         self.lineEdits['5a'].resize(50, 32)
+        self.lineEdits['5a'].setText("1")
 
         labels['6a'] = QLabel(self)
         labels['6a'].setText('Bonus ataque carregado')
@@ -122,6 +126,7 @@ class MainApp(QWidget):
         self.lineEdits['6a'] = QLineEdit(self)
         self.lineEdits['6a'].move(140, 235)
         self.lineEdits['6a'].resize(50, 32)
+        self.lineEdits['6a'].setText("1")
 
         labels['7a'] = QLabel(self)
         labels['7a'].setText('Bonus Skill')
@@ -129,6 +134,7 @@ class MainApp(QWidget):
         self.lineEdits['7a'] = QLineEdit(self)
         self.lineEdits['7a'].move(140, 275)
         self.lineEdits['7a'].resize(50, 32)
+        self.lineEdits['7a'].setText("1")
 
         labels['8a'] = QLabel(self)
         labels['8a'].setText('Bonus Ultimate')
@@ -136,6 +142,7 @@ class MainApp(QWidget):
         self.lineEdits['8a'] = QLineEdit(self)
         self.lineEdits['8a'].move(140, 315)
         self.lineEdits['8a'].resize(50, 32)
+        self.lineEdits['8a'].setText("1")
 
 
         labels['1b'] = QLabel(self)
@@ -144,6 +151,7 @@ class MainApp(QWidget):
         self.lineEdits['1b'] = QLineEdit(self)
         self.lineEdits['1b'].move(370, 35)
         self.lineEdits['1b'].resize(50, 32)
+        self.lineEdits['1b'].setText("1")
 
         labels['2b'] = QLabel(self)
         labels['2b'].setText('Modificador ataque carregado')
@@ -151,6 +159,7 @@ class MainApp(QWidget):
         self.lineEdits['2b'] = QLineEdit(self)
         self.lineEdits['2b'].move(370, 75)
         self.lineEdits['2b'].resize(50, 32)
+        self.lineEdits['2b'].setText("1")
 
         labels['3b'] = QLabel(self)
         labels['3b'].setText('Modificador de skill')
@@ -158,6 +167,7 @@ class MainApp(QWidget):
         self.lineEdits['3b'] = QLineEdit(self)
         self.lineEdits['3b'].move(370, 115)
         self.lineEdits['3b'].resize(50, 32)
+        self.lineEdits['3b'].setText("1")
 
         labels['4b'] = QLabel(self)
         labels['4b'].setText('Modificador de ultimate')
@@ -165,6 +175,7 @@ class MainApp(QWidget):
         self.lineEdits['4b'] = QLineEdit(self)
         self.lineEdits['4b'].move(370, 155)
         self.lineEdits['4b'].resize(50, 32)
+        self.lineEdits['4b'].setText("1")
 
 
         labels['1c'] = QLabel(self)
@@ -173,6 +184,7 @@ class MainApp(QWidget):
         self.lineEdits['1c'] = QLineEdit(self)
         self.lineEdits['1c'].move(580, 35)
         self.lineEdits['1c'].resize(50, 32)
+        self.lineEdits['1c'].setText("1")
 
         labels['2c'] = QLabel(self)
         labels['2c'].setText('Ataque bonus')
@@ -180,6 +192,7 @@ class MainApp(QWidget):
         self.lineEdits['2c'] = QLineEdit(self)
         self.lineEdits['2c'].move(580, 75)
         self.lineEdits['2c'].resize(50, 32)
+        self.lineEdits['2c'].setText("1")
 
         labels['3c'] = QLabel(self)
         labels['3c'].setText('Taxa Crítica Bonus')
@@ -187,6 +200,7 @@ class MainApp(QWidget):
         self.lineEdits['3c'] = QLineEdit(self)
         self.lineEdits['3c'].move(580, 115)
         self.lineEdits['3c'].resize(50, 32)
+        self.lineEdits['3c'].setText("1")
 
         labels['4c'] = QLabel(self)
         labels['4c'].setText('Dano Crítico Bonus')
@@ -194,6 +208,7 @@ class MainApp(QWidget):
         self.lineEdits['4c'] = QLineEdit(self)
         self.lineEdits['4c'].move(580, 155)
         self.lineEdits['4c'].resize(50, 32)
+        self.lineEdits['4c'].setText("1")
 
         labels['5c'] = QLabel(self)
         labels['5c'].setText('Bonus ataque normal')
@@ -201,6 +216,7 @@ class MainApp(QWidget):
         self.lineEdits['5c'] = QLineEdit(self)
         self.lineEdits['5c'].move(580, 195)
         self.lineEdits['5c'].resize(50, 32)
+        self.lineEdits['5c'].setText("1")
 
         labels['6c'] = QLabel(self)
         labels['6c'].setText('Bonus ataque carregado')
@@ -208,6 +224,7 @@ class MainApp(QWidget):
         self.lineEdits['6c'] = QLineEdit(self)
         self.lineEdits['6c'].move(580, 235)
         self.lineEdits['6c'].resize(50, 32)
+        self.lineEdits['6c'].setText("1")
 
         labels['7c'] = QLabel(self)
         labels['7c'].setText('Bonus de skill')
@@ -215,6 +232,7 @@ class MainApp(QWidget):
         self.lineEdits['7c'] = QLineEdit(self)
         self.lineEdits['7c'].move(580, 275)
         self.lineEdits['7c'].resize(50, 32)
+        self.lineEdits['7c'].setText("1")
 
         labels['8c'] = QLabel(self)
         labels['8c'].setText('Bonus de ultimate')
@@ -222,6 +240,7 @@ class MainApp(QWidget):
         self.lineEdits['8c'] = QLineEdit(self)
         self.lineEdits['8c'].move(580, 315)
         self.lineEdits['8c'].resize(50, 32)
+        self.lineEdits['8c'].setText("1")
 
 
 
@@ -235,6 +254,7 @@ class MainApp(QWidget):
         self.lineEdits['dano1'] = QLineEdit(self)
         self.lineEdits['dano1'].move(140, 500)
         self.lineEdits['dano1'].resize(50, 32)
+        self.lineEdits['dano1'].setText("0")
 
         labels['dano2'] = QLabel(self)
         labels['dano2'].setText('Dano Ataque Carregado')
@@ -242,6 +262,7 @@ class MainApp(QWidget):
         self.lineEdits['dano2'] = QLineEdit(self)
         self.lineEdits['dano2'].move(140, 540)
         self.lineEdits['dano2'].resize(50, 32)
+        self.lineEdits['dano2'].setText("0")
 
         labels['dano3'] = QLabel(self)
         labels['dano3'].setText('Dano de Skill')
@@ -249,6 +270,7 @@ class MainApp(QWidget):
         self.lineEdits['dano3'] = QLineEdit(self)
         self.lineEdits['dano3'].move(140, 580)
         self.lineEdits['dano3'].resize(50, 32)
+        self.lineEdits['dano3'].setText("0")
 
         labels['dano4'] = QLabel(self)
         labels['dano4'].setText('Dano de Ultimate')
@@ -256,6 +278,7 @@ class MainApp(QWidget):
         self.lineEdits['dano4'] = QLineEdit(self)
         self.lineEdits['dano4'].move(140,620)
         self.lineEdits['dano4'].resize(50, 32)
+        self.lineEdits['dano4'].setText("0")
 
 
         labels['DPS1'] = QLabel(self)
@@ -264,6 +287,7 @@ class MainApp(QWidget):
         self.lineEdits['DPS1'] = QLineEdit(self)
         self.lineEdits['DPS1'].move(370, 500)
         self.lineEdits['DPS1'].resize(50, 32)
+        self.lineEdits['DPS1'].setText("0")
 
 
         labels['DPS2'] = QLabel(self)
@@ -272,6 +296,7 @@ class MainApp(QWidget):
         self.lineEdits['DPS2'] = QLineEdit(self)
         self.lineEdits['DPS2'].move(370, 540)
         self.lineEdits['DPS2'].resize(50, 32)
+        self.lineEdits['DPS2'].setText("0")
 
         labels['DPS3'] = QLabel(self)
         labels['DPS3'].setText('Dano efetivo Skill')
@@ -279,6 +304,7 @@ class MainApp(QWidget):
         self.lineEdits['DPS3'] = QLineEdit(self)
         self.lineEdits['DPS3'].move(370, 580)
         self.lineEdits['DPS3'].resize(50, 32)
+        self.lineEdits['DPS3'].setText("0")
 
         labels['DPS4'] = QLabel(self)
         labels['DPS4'].setText('Dano efetivo Ultimate')
@@ -286,20 +312,16 @@ class MainApp(QWidget):
         self.lineEdits['DPS4'] = QLineEdit(self)
         self.lineEdits['DPS4'].move(370, 620)
         self.lineEdits['DPS4'].resize(50, 32)
+        self.lineEdits['DPS4'].setText("0")
 
+
+
+
+        #self.create_widgets1()
 
 
 
         self.create_widgets1()
-
-
-
-
-
-
-
-
-
 
 
 
@@ -312,10 +334,23 @@ class MainApp(QWidget):
 
 
 
+        telainicial = QPushButton('tela inicial', self)
+        telainicial.setGeometry(580, 560, 75, 50)
+        telainicial.setStyleSheet('background-color:white')
+        telainicial.setIcon(QIcon("xiangling.png"))
+        telainicial.clicked.connect(self.tela)
+
+
+
         self.label = QLabel("   Esperando", self)
         self.label.setGeometry(580, 680, 75, 20)
         self.label.setStyleSheet('background-color:green')
         self.label.setFont(QFont("Times New Roman", 10))
+
+    def tela(self):
+        self.login = LoginWindow()
+        self.login.show()
+        self.close()
 
     def clicked_bnt(self):
         self.label.setText("    calculado")
@@ -386,51 +421,6 @@ class MainApp(QWidget):
         print(modcrit)
 
 
-
-
-
-        # btn.clicked.connect(self.dano)
-
-    # def dano(self):
-    #
-    #     bonus1 = bonusataquen + Abonusataquen
-    #     bonus2 = bonusataquec + Abonusataquec
-    #     bonus3 = bonusskill + Abonusskill
-    #     bonus4 = bonusultimate + Abonusultimate
-    #
-    #
-    #     effatk = ataqueverde//ataquebranco
-    #     modcrit = taxacritica * danocritico//100
-    #     modbonus = (bonus1 + bonus2 + bonus3 + bonus4)//4
-    #
-    #
-    #
-    #     Dano1 = (ataquebranco + ataqueverde) * talento1n//100 * (1+(bonus1//100)) * (1+(danocritico//100))
-    #     Dano2 = (ataquebranco + ataqueverde) * talento1c//100 * (1+(bonus2//100)) * (1+(danocritico//100))
-    #     Dano3 = (ataquebranco + ataqueverde) * talento2//100 * (1+(bonus3//100)) * (1+(danocritico//100))
-    #     Dano4 = (ataquebranco + ataqueverde) * talento3//100 * (1+(bonus4//100)) * (1+(danocritico//100))
-    #
-    #     DPS1 = (ataquebranco + ataqueverde) * talento1n//100 * (1+(bonus1//100)) * (1+(modcrit//100))
-    #     DPS2 = (ataquebranco + ataqueverde) * talento1c//100 * (1+(bonus2//100)) * (1+(modcrit//100))
-    #     DPS3 = (ataquebranco + ataqueverde) * talento2//100 * (1+(bonus3//100)) * (1+(modcrit//100))
-    #     DPS4 = (ataquebranco + ataqueverde) * talento3//100 * (1+(bonus4//100)) * (1+(modcrit//100))
-    #
-    #     print(Dano1)
-    #     print(Dano2)
-    #     print(Dano3)
-    #     print(Dano4)
-    #
-    #     print(DPS1)
-    #     print(DPS2)
-    #     print(DPS3)
-    #     print(DPS4)
-
-
-
-
-
-
-
 class LoginWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -446,13 +436,20 @@ class LoginWindow(QWidget):
         labels = {}
         self.lineEdits = {}
 
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="1234",
+            database="testdatabase"
+        )
+
         labels['Usuário'] = QLabel('Usuário')
         labels['Senha'] = QLabel('Senha')
-        labels['Usuário'].setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Fixed)
+        labels['Usuário'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         labels['Senha'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
-        labels['usu'] = QLabel(f'Usuário correto: {usuario}')
-        labels['pass'] = QLabel(f'Senha correta: {senha}')
+        labels['usu'] = QLabel(f'Usuário: {"oi"}')
+        labels['pass'] = QLabel(f'Senha: {123}')
         layout.addWidget(labels['usu'], 2, 2, 1, 1)
         layout.addWidget(labels['pass'], 2, 3, 1, 1)
 
@@ -460,20 +457,28 @@ class LoginWindow(QWidget):
         self.lineEdits['Senha'] = QLineEdit()
         self.lineEdits['Senha'].setEchoMode(QLineEdit.EchoMode.Password)
 
-        layout.addWidget(labels['Usuário'],            0, 0, 1, 1)
-        layout.addWidget(self.lineEdits['Usuário'],    0, 1, 1, 3)
+        layout.addWidget(labels['Usuário'], 0, 0, 1, 1)
+        layout.addWidget(self.lineEdits['Usuário'], 0, 1, 1, 3)
 
-        layout.addWidget(labels['Senha'],              1, 0, 1, 1)
-        layout.addWidget(self.lineEdits['Senha'],      1, 1, 1, 3)
+        layout.addWidget(labels['Senha'], 1, 0, 1, 1)
+        layout.addWidget(self.lineEdits['Senha'], 1, 1, 1, 3)
 
         button_login = QPushButton('&Log in', clicked=self.checkCreditial)
         layout.addWidget(button_login, 2, 4, 1, 1)
+
+        button_registro = QPushButton('&Registrar', clicked=self.checkRegistro)
+        layout.addWidget(button_registro, 2, 4, 3, 1)
 
         self.status = QLabel('')
         self.status.setStyleSheet('font-size: 25px; color:red')
         layout.addWidget(self.status, 3, 0, 1, 3)
 
-        #self.connectToDB()
+    def checkRegistro(self):
+        self.mainregistro = DATABASE()
+        self.mainregistro.show()
+        self.close()
+
+    # self.connectToDB()
 
     # def connectToDB(self):
     #
@@ -485,14 +490,19 @@ class LoginWindow(QWidget):
     #         self.status.setText('Connection failed')
 
     def checkCreditial(self):
-        username = self.lineEdits['Usuário'].text()
-        password = self.lineEdits['Senha'].text()
+        username2 = self.lineEdits['Usuário'].text()
+        password2 = self.lineEdits['Senha'].text()
 
+        mycursor = self.db.cursor()
 
+        # mycursor.execute("SELECT user, password FROM DATA4 WHERE user = username2, password = password2")
+        # mycursor.execute("SELECT * FROM DATA4 WHERE user = %s, password = %s", (username2, password2,))
+        mycursor.execute("SELECT user, password FROM DATA4 WHERE user = %s", (username2,))
+        varuser = mycursor.fetchone()
 
-        if usuario == username:
-            if password == senha:
-                time.sleep(1)
+        if varuser is not None:
+            if password2 == varuser[1]:
+                # time.sleep(1)
                 self.mainApp = MainApp()
                 self.mainApp.show()
                 self.close()
@@ -502,22 +512,71 @@ class LoginWindow(QWidget):
             self.status.setText('Usuário errado')
 
 
+class DATABASE(QWidget):
+    def __init__(self):
+        super().__init__()
+        #status = QLabel('Status', parent=self)
+        self.setWindowTitle("Janela de Registro")
+        self.window_width, self.window_height = 600, 150
+        self.setFixedSize(self.window_width, self.window_height)
+        self.setWindowIcon(QIcon("xiangling.png"))
+        self.setWindowTitle("Calculadora Genshin Avançada")
 
-        # query = QSqlQuery()
-        # query.prepare('Insert INTO person (Usuário)')
-        # query.bindValue(':Usuário', username)
-        # query.exec()
-        #
-        # if query.first():
-        #     if query.value('Senha') == password:
-        #         time.sleep(1)
-        #         self.mainApp = MainApp()
-        #         self.mainApp.show()
-        #         self.close()
-        #     else:
-        #         self.status.setText('Senha está errada')
-        # else:
-        #     self.setText('Usuário não foi encontrado')
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+
+        labels = {}
+        self.lineEdits = {}
+
+        labels['Novo Usuário'] = QLabel('Novo Usuário')
+        labels['Nova Senha'] = QLabel('Nova Senha')
+        labels['Novo Usuário'].setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Fixed)
+        labels['Nova Senha'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+
+        #labels['usu'] = QLabel(f'Novo Usuário: {usuario}')
+        #labels['pass'] = QLabel(f'Senha correta: {senha}')
+        #layout.addWidget(labels['usu'], 2, 2, 1, 1)
+        #layout.addWidget(labels['pass'], 2, 3, 1, 1)
+
+        self.lineEdits['Novo Usuário'] = QLineEdit()
+        self.lineEdits['Nova Senha'] = QLineEdit()
+        self.lineEdits['Nova Senha'].setEchoMode(QLineEdit.EchoMode.Password)
+
+        layout.addWidget(labels['Novo Usuário'],            0, 0, 1, 1)
+        layout.addWidget(self.lineEdits['Novo Usuário'],    0, 1, 1, 3)
+
+        layout.addWidget(labels['Nova Senha'],              1, 0, 1, 1)
+        layout.addWidget(self.lineEdits['Nova Senha'],      1, 1, 1, 3)
+
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="1234",
+            database="testdatabase"
+        )
+
+        confirm_register = QPushButton('&Confirmar Registro', clicked=self.register)
+        layout.addWidget(confirm_register, 2, 4, 1, 1)
+
+    def register(self):
+        usernamenew = self.lineEdits['Novo Usuário'].text()
+        pasw = self.lineEdits['Nova Senha'].text()
+
+        tipo = 'F'
+
+        print(usernamenew)
+        print(pasw)
+
+        mycursor = self.db.cursor()
+
+        #mycursor.execute("INSERT INTO DATA4 user, password FROM DATA4 WHERE user = %s", (username2,))
+        mycursor.execute("INSERT INTO DATA4 (user, password, type, created) VALUES (%s,%s,%s,%s)", (usernamenew, pasw, tipo, datetime.now(),))
+        self.db.commit()
+
+        self.login = LoginWindow()
+        self.login.show()
+        self.close()
 
 
 if __name__ == '__main__':
